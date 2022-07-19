bbd2f642f79824b42e1a3edd1c372d9106633777
davidnene <davienesh4@gmail.com>
2022-07-19 14:28:53 +0300

    commit tree dcf90ce340cf84103f68ee875683d137922ab0cf

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+
+function App() {
+    const [dog, setDog] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(image => setDog(image.message))
+    }, [])
+
+    if (!dog) {return <p>Loading...</p>}
+    else {
+     return <img src = {dog} alt = "A Random Dog"/>}
+        
+    
+
+}
+
+
+
+export default App;
